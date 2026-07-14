700e7013e96c84ab741097c2a951f4c3ef14831b
Yassin Abdelnader <abdelnay@shernet.sheridancollege.ca>
2026-07-14 15:43:19 -0400

    Added input of movment to keyboard

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInput : MonoBehaviour
+{
+    public float speed;
+
+
+
+
+    private Vector2 movmentDirection = Vector2.zero;
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position +=(Vector3) movmentDirection*speed * Time.deltaTime;
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movmentDirection = context.ReadValue<Vector2>();
+    }    
+}
