a5b8a70c558a0cf584527cada0f016540771e59a
Yassin Abdelnader <abdelnay@shernet.sheridancollege.ca>
2026-07-28 16:43:31 -0400

    manging LineMaker

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    public Animator heroAnimator;
+    public bool isMoving = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        heroAnimator.SetBool("isMoving", isMoving);
+    }
+}
